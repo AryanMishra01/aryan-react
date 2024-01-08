56b1f92db4d8ff181569fd7910d3b15bcb92bb45
AryanMishra01 <aryanmishrareact@gmail.com>
2024-01-08 15:31:55 +0530

    Day_01

--- App.js
+++ App.js
@@ -5,10 +5,15 @@
 //     "Hello World from React!"
 //     )
 // console.log(heading) //object
-
 // const root = ReactDOM.createRoot(document.getElementById("root"))
-
 // root.render(heading);
+
+// ReactElement(Object) => HTML(Browser understands)
+//createElement has three Argument"
+   // a. html tag
+   // b. attribute
+   // c. children: 1. single child  
+   //              2. multiple child=> pass as an array as below.
 
 const heading = React.createElement(
     "div",
@@ -26,4 +31,4 @@ React.createElement("h2", {id: "heading2"}, "This is H2 tag")
     // Since above way is very tds and hard to track the structure
     // we will use JSX which will simplfy this code.
 const root =ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+root.render(heading);
